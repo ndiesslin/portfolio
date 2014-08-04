41563347f36fac64edcc94f23f6aed82a5a65e90
nicholas diesslin <nicholasdiesslin@gmail.com>
2014-08-04 12:19:29 -0500

    v1.1

--- index.php
+++ index.php
@@ -182,7 +182,7 @@ function get_data($var) {
             <h2 id="about-text">
                 <span id="about-1">My name is <strong id="nicholas">Nicholas Diesslin</strong>, I’m a <strong>web designer</strong> from Minneapolis.</span>
                 <br/> 
-                 <span id="about-2">Although I enjoy <em>web design</em>, I also enjoy doing <em>graphic design</em>. Typography is my favorite part of design. Art is a crucial aspect of my life, as it inspires most of my creativity. I like making, playing, and listening to music; my favorite instrument to play is the saxophone. I prefer Marvel over D.C., but Superman is my favorite superhero.</span>
+                 <span id="about-2">I am a lover of <em>web design</em>, I also enjoy doing <em>graphic design</em>. Typography is my favorite part of design. Art is a crucial aspect of my life, as it inspires most of my creativity. I like making, playing, and listening to music; my favorite instrument to play is the saxophone. I prefer Marvel over D.C., but Superman is my favorite superhero.</span>
             </h2>
         </section>
         <section id="projects">
